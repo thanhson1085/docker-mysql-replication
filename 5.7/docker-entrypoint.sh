0287032e4d58f284fa399004efacc6b24a5362a7
thanhson1085 <sonnst@sigma-solutions.eu>
2016-10-10 11:10:24 +0000

    add setup master slave in entrypoint sh :elephant:

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -22,6 +22,48 @@ _datadir() {
 	"$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }'
 }
 
+CONF_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
+
+# Set MySQL REPLICATION - MASTER
+if [ -n "${REPLICATION_MASTER}" ]; then
+    echo "=> Configuring MySQL replication as master (1/2) ..."
+    if [ ! -f /tmp/replication_set.1 ]; then
+        RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
+        echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
+        cat ${CONF_FILE} > /tmp/mysqld.cnf && \
+            echo "bind-address = 0.0.0.0" >> /tmp/mysqld.cnf && \
+            echo "server-id = ${RAND}" >> /tmp/mysqld.cnf && \
+            echo "log-bin = mysql-bin" >> /tmp/mysqld.cnf && \
+            mv /tmp/mysqld.cnf ${CONF_FILE}
+        touch /tmp/replication_set.1
+    else
+        echo "=> MySQL replication master already configured, skip"
+    fi
+fi
+
+# Set MySQL REPLICATION - SLAVE
+if [ -n "${REPLICATION_SLAVE}" ]; then
+    echo "=> Configuring MySQL replication as slave (1/2) ..."
+    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
+        if [ ! -f /tmp/replication_set.1 ]; then
+            RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
+            echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
+            cat ${CONF_FILE} > /tmp/mysqld.cnf && \
+                echo "bind-address = 0.0.0.0" >> /tmp/mysqld.cnf && \
+                echo "server-id = ${RAND}" >> /tmp/mysqld.cnf && \
+                echo "log-bin = mysql-bin" >> /tmp/mysqld.cnf && \
+                mv /tmp/mysqld.cnf ${CONF_FILE}
+            touch /tmp/replication_set.1
+        else
+            echo "=> MySQL replication slave already configured, skip"
+        fi
+    else
+        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
+        exit 1
+    fi
+fi
+
+
 # allow the container to be started with `--user`
 if [ "$1" = 'mysqld' -a -z "$wantHelp" -a "$(id -u)" = '0' ]; then
 	DATADIR="$(_datadir "$@")"
@@ -104,30 +146,35 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 		fi
 
-        CONFIG_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
-
+        # Set MySQL REPLICATION - MASTER
         if [ -n "${REPLICATION_MASTER}" ]; then
-            echo "=> Configuring MySQL replication as master (1/2) ..."
-            if [ ! -f /replication_set.1 ]; then
-                RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-                echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-                sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-                sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-                touch /replication_set.1
+            echo "=> Configuring MySQL replication as master (2/2) ..."
+            if [ ! -f /tmp/replication_set.2 ]; then
+                echo "=> Creating a log user ${REPLICATION_USER}:${REPLICATION_PASS}"
+                # delete user if exists
+                mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "DELETE FROM mysql.user WHERE User = '${REPLICATION_USER}'" && \
+                    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES" && \
+                    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '${REPLICATION_USER}'@'%' IDENTIFIED BY '${REPLICATION_PASS}'"
+                mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT REPLICATION SLAVE ON *.* TO '${REPLICATION_USER}'@'%'"
+                mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "reset master"
+                echo "=> Done!"
+                touch /tmp/replication_set.2
             else
                 echo "=> MySQL replication master already configured, skip"
             fi
         fi
 
+        # Set MySQL REPLICATION - SLAVE
         if [ -n "${REPLICATION_SLAVE}" ]; then
-            echo "=> Configuring MySQL replication as slave (1/2) ..."
+            echo "=> Configuring MySQL replication as slave (2/2) ..."
             if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
-                if [ ! -f /replication_set.1 ]; then
-                    RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-                    echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-                    sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-                    sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-                    touch /replication_set.1
+                if [ ! -f /tmp/replication_set.2 ]; then
+                    echo "=> Setting master connection info on slave"
+                    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "stop slave"
+                    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CHANGE MASTER TO MASTER_HOST='${MYSQL_PORT_3306_TCP_ADDR}',MASTER_USER='${MYSQL_ENV_REPLICATION_USER}',MASTER_PASSWORD='${MYSQL_ENV_REPLICATION_PASS}',MASTER_PORT=${MYSQL_PORT_3306_TCP_PORT}, MASTER_CONNECT_RETRY=30"
+                    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "start slave"
+                    echo "=> Done!"
+                    touch /tmp/replication_set.2
                 else
                     echo "=> MySQL replication slave already configured, skip"
                 fi
